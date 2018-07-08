974e1bc6814b1364510751e5164026e69f74c53c
Ganesh Radhakrishnan <ganrad01@gmail.com>
2018-07-07 21:57:11 -0700

    Updated the PO Model.
    Updated the discount and orderTotal fields.

--- src/main/java/ocp/s2i/springboot/rest/model/PurchaseOrder.java
+++ src/main/java/ocp/s2i/springboot/rest/model/PurchaseOrder.java
@@ -98,6 +98,7 @@ public class PurchaseOrder {
     }
 
 // Uncomment this method for retrieving order total !!!
+	/**
     public Float getDiscountAmount() {
 	return ( (this.price * this.quantity) * ( Float.parseFloat(this.dcode) / 100 ) );
     } 
@@ -105,7 +106,7 @@ public class PurchaseOrder {
     public Float getOrderTotal() {
 	return ( (this.price * this.quantity) - getDiscountAmount() );
     }
-
+*/
     public String toString() {
 	StringBuffer sb = new StringBuffer();
 	sb.append(
